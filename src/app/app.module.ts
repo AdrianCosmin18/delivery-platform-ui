1421efbf6db19c47f3c2a3aaf2aaab0a3a852f4a
Adrian Cosmin Nedelcu <cosminadrian1304@gmail.com>
2023-04-10 20:32:30 +0300

    add primeng dependecies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import {MenubarModule} from "primeng/menubar";
+import {MultiSelectModule} from "primeng/multiselect";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MenubarModule,
+    MultiSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
